754aa3d5f6397b4aa266e2c93ed3f45975dede21
erkinchik <erkin.berdibaev@gmail.com>
2023-12-17 22:54:34 +0200

    int to roman with hash table

---
+++ leetcode-12.js
@@ -0,0 +1,32 @@
+
+
+var intToRoman = function(num) {
+    let res = '';
+    const map = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    }
+
+    for (const symbol in map) {
+        while(map[symbol] <= num){
+         res += symbol
+         num -= map[symbol]
+        }
+    }
+
+
+    return res
+};
+
+console.log(intToRoman(20));
